8b3a7058f111b574c9b3e062987989fb2d52df46
yuzc <651977326@qq.com>
2019-01-18 10:33:35 +0800

    git

--- springboot-netty-demo/src/main/java/com/kcren/demo/Application.java
+++ springboot-netty-demo/src/main/java/com/kcren/demo/Application.java
@@ -1,30 +1,20 @@
 package com.kcren.demo;
 
 
-import com.zhongfeng.sms.base.common.seq.SnowFlake;
+
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Random;
 
 /**
  * @author yuzc
  */
 @SpringBootApplication
 @Slf4j
-@MapperScan(value = {"com.zhongfeng.sms.persistence.auto.mapper"})
 public class Application {
 
-    @Bean
-    public SnowFlake getIdWorker() {
-        int workerId = new Random().nextInt(20);
-        int dataCenterId = new Random().nextInt(20);
-        SnowFlake worker = new SnowFlake(workerId, dataCenterId);
-        return worker;
-    }
+
     /**
      * Start
      */
